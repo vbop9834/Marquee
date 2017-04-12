9470ac4e19ed6195b1dbc985ed631779c8ea4744
Jeremy Bellows <JeremyBellows@gmail.com>
2017-04-12 09:59:53 -0600

    Added tests, assertions, and actions

--- marquee.tests/marquee.tests.fs
+++ marquee.tests/marquee.tests.fs
@@ -3,7 +3,7 @@ module marquee.tests
 open marquee
 open testManager
 
-let amountOfBrowsers = 5
+let amountOfBrowsers = 10
 let resultsFunction = fun results ->
   let printResultsToScreen (testDescription, testResult) =
     printfn "%s" testDescription
@@ -13,11 +13,13 @@ let resultsFunction = fun results ->
       0
     | TestFailed ex -> 
       printfn "Failed"
+      printfn "%s" ex.Message
       printfn "%s" ex.StackTrace
       1
   let testExitCodes = 
     results 
     |> List.map printResultsToScreen
+  printfn "Tests Executed - %i" <| List.length results
   System.Console.ReadLine() |> ignore
   match testExitCodes |> List.exists(fun exitCode -> exitCode = 1) with
   | true -> 1
@@ -30,6 +32,11 @@ let testPageUrl = "http://lefthandedgoat.github.io/canopy/testpages/"
 let buttonClickedSelector = "#button_clicked"
 let buttonSelector = "#button"
 let welcomeSelector = "#welcome"
+let firstNameSelector = "#firstName"
+let lastNameSelector = "#lastName"
+let tableSelector = "#value_list td"
+let itemListSelector = "#item_list option"
+let checkboxSelector = "#checkbox"
 
 "Navigating to the test page and clicking a button should change the button text" -- fun browser ->
   browser.Url testPageUrl
@@ -38,6 +45,52 @@ let welcomeSelector = "#welcome"
   buttonClickedSelector |> browser.ElementTextEquals "button clicked"
   browser.Displayed welcomeSelector
   welcomeSelector |> browser.ElementTextEquals "Welcome"
+
+"On the test page, there should be a first and last name" -- fun browser ->
+  browser.Url testPageUrl
+  firstNameSelector |> browser.ElementTextEquals "John"
+  lastNameSelector |> browser.ElementTextEquals "Doe"
+
+"Should be able to clear first name input" -- fun browser ->
+  browser.Url testPageUrl
+  firstNameSelector |> browser.ClearInput
+  firstNameSelector |> browser.ElementTextEquals ""
+
+"Should be able to set last name input" -- fun browser ->
+  browser.Url testPageUrl
+  lastNameSelector |> browser.ClearInput
+  lastNameSelector |> browser.ElementTextEquals ""
+  lastNameSelector |> browser.SetInput "Smith"
+  lastNameSelector |> browser.ElementTextEquals "Smith"
+
+"Should be able to identify an item in a table" -- fun browser ->
+  browser.Url testPageUrl
+  tableSelector |> browser.TextExistsInElements "Value 1"
+  tableSelector |> browser.TextExistsInElements "Value 2"
+  tableSelector |> browser.TextExistsInElements "Value 3"
+  tableSelector |> browser.TextExistsInElements "Value 4"
+  tableSelector |> browser.TextExistsInElements "Value 4"
+
+"Should be able to identify an item in a select list" -- fun browser ->
+  browser.Url testPageUrl
+  itemListSelector |> browser.TextExistsInElements "Item 1"
+  itemListSelector |> browser.TextExistsInElements "Item 2"
+  itemListSelector |> browser.TextExistsInElements "Item 3"
+  itemListSelector |> browser.TextExistsInElements "Item 4"
+
+"Should be able to check a checkbox" -- fun browser ->
+  browser.Url testPageUrl
+  checkboxSelector |> browser.AreElementsUnChecked
+  checkboxSelector |> browser.CheckElements
+  checkboxSelector |> browser.AreElementsChecked
+
+"Should be able to uncheck a checkbox" -- fun browser ->
+  browser.Url testPageUrl
+  checkboxSelector |> browser.AreElementsUnChecked
+  checkboxSelector |> browser.CheckElements
+  checkboxSelector |> browser.AreElementsChecked
+  checkboxSelector |> browser.UnCheckElements
+  checkboxSelector |> browser.AreElementsUnChecked
 
 testManager.RunTests ()
 let exitCode = testManager.ReportResults ()
--- marquee/marquee.fs
+++ marquee/marquee.fs
@@ -11,6 +11,10 @@ module marquee
   exception WaitTimeoutException
   exception NoWebElementsDisplayed of string
   exception WebElementsDoNotMatchSuppliedText of string
+  exception WebElementIsReadOnly of IWebElement
+  exception WebElementSelectDoesNotContainText of string
+  exception WebElementIsNotChecked of IWebElement
+  exception WebElementIsChecked of IWebElement
 
   let private wait (timeout : int) (continueFunction : ContinueFunction<'T>) =
     let stopwatch = System.Diagnostics.Stopwatch.StartNew()
@@ -98,15 +102,76 @@ module marquee
     member this.Url (url : string) =
       this.instance.Navigate().GoToUrl(url)
 
-    member this.ElementTextEquals testText cssSelector =
-      let elements = this.FindElements cssSelector
-      let readText = fun (element : IWebElement) ->
+    static member private ReadText (element : IWebElement) =
         match element.TagName.ToLower() with
         | "input" | "textarea" -> element.GetAttribute("value")
         | _ -> 
           element.Text
-      match elements |> Array.filter(fun element -> (readText element) <> testText) with
+
+    member this.ElementTextEquals testText cssSelector =
+      let elements = this.FindElements cssSelector
+      //TODO replace filter with map filter operation
+      //for efficiency
+      match elements |> Array.filter(fun element -> (Browser.ReadText element) <> testText) with
       | [||] -> () 
       | elements -> 
-        let elements = elements |> Array.map(fun element -> readText element)
+        let elements = elements |> Array.map(fun element -> Browser.ReadText element)
         raise <| WebElementsDoNotMatchSuppliedText (sprintf "%s is not found in %A" testText elements)
+
+    member this.ClearInput cssSelector =
+      let elements = this.FindElements cssSelector
+      let clear (element : IWebElement) =
+        match element.GetAttribute("readonly") with
+        | "true" -> raise <| WebElementIsReadOnly element
+        | _ -> element.Clear()
+      elements |> Array.iter clear
+
+    member this.SetInput text cssSelector =
+      let elements = this.FindElements cssSelector
+      let clear (element : IWebElement) =
+        match element.GetAttribute("readonly") with
+        | "true" -> raise <| WebElementIsReadOnly element
+        | _ -> 
+          element.Clear()
+          element.SendKeys(text)
+      elements |> Array.iter clear
+
+    member this.TextExistsInElements text cssSelector =
+      let elements = this.FindElements cssSelector
+      let testElementText element =
+        (Browser.ReadText element) <> text
+      match elements  |> Array.tryFind testElementText with 
+      | Some elementThatContainsText -> ()
+      | None -> raise <| WebElementSelectDoesNotContainText text 
+      
+    member this.CheckElements cssSelector =
+      let elements = this.FindElements cssSelector
+      let check (element : IWebElement) =
+        match element.Selected with
+        | true -> ()
+        | false -> element.Click()
+      elements |> Array.iter check
+
+    member this.UnCheckElements cssSelector =
+      let elements = this.FindElements cssSelector
+      let check (element : IWebElement) =
+        match element.Selected with
+        | true -> element.Click()
+        | false -> ()
+      elements |> Array.iter check
+
+    member this.AreElementsChecked cssSelector =
+      let elements = this.FindElements cssSelector
+      let testElement (element : IWebElement) =
+        match element.Selected with
+        | true -> ()
+        | false -> raise <| WebElementIsNotChecked element
+      elements |> Array.iter testElement
+
+    member this.AreElementsUnChecked cssSelector =
+      let elements = this.FindElements cssSelector
+      let testElement (element : IWebElement) =
+        match element.Selected with
+        | true -> raise <| WebElementIsChecked element
+        | false -> ()
+      elements |> Array.iter testElement
